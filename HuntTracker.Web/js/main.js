a68ce182be93a7e287d5a1bdaac4f08964eadff5
Vegard Gillestad <vegard.gillestad@gmail.com>
2015-01-08 19:57:00 +0100

    first commit

---
+++ HuntTracker.Web/js/main.js
@@ -0,0 +1,10 @@
+﻿angular.module("hunters", ['gettext', 'openlayers-directive'])
+
+    .controller("HuntersCtrl", function ($scope) {
+        $scope.name = "Vegard";
+        $scope.names = ["Kjell", "Geir", "Petter"];
+    })
+
+    .controller("MapCtrl", function ($scope) {
+        $scope.name = "Map";
+    });
---
+++ HuntTracker.Web/js/services.js
@@ -0,0 +1,35 @@
+﻿angular.module('PushAppSources', ['ngResource'])
+    .factory('TableSources', function ($resource) {
+        var sources = $resource("/api/tablepushwidgets/:widgetId", null, {
+            getAll: { method: "GET", isArray: true },
+            getById: { method: "GET", params: { widgetId: "@widgetId" } },
+            add: { method: "POST" },
+            update: { method: "PUT" }
+        });
+
+        return sources;
+    })
+
+    .factory('MapSources', function ($resource) {
+        var sources = $resource("/api/mappushwidgets/:widgetId", null, {
+            getAll: { method: "GET", isArray: true },
+            getById: { method: "GET", params: { widgetId: "@widgetId" } },
+            add: { method: "POST" },
+            update: { method: "PUT" }
+        });
+
+        return sources;
+    })
+
+    .factory('ChartSources', function ($resource) {
+        var sources = $resource("/api/chartpushwidgets/:widgetId", null, {
+            getAll: { method: "GET", isArray: true },
+            getById: { method: "GET", params: { widgetId: "@widgetId" } },
+            add: { method: "POST" },
+            update: { method: "PUT" }
+        });
+
+        return sources;
+    })
+
+;
