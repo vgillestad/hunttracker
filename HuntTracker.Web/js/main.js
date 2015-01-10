599f870f1f8b5e44061e1e2bb81e30c171815795
Vegard Gillestad <vegard.gillestad@gmail.com>
2015-01-10 09:10:50 +0100

    implemented open layers angular directive

--- HuntTracker.Web/js/main.js
+++ HuntTracker.Web/js/main.js
@@ -1,10 +1,38 @@
-﻿angular.module("hunters", ['gettext', 'openlayers-directive'])
+﻿angular.module("HuntTracker", ["gettext", "openlayers-directive"])
 
-    .controller("HuntersCtrl", function ($scope) {
+    .controller("MenuCtrl", function ($scope) {
         $scope.name = "Vegard";
         $scope.names = ["Kjell", "Geir", "Petter"];
     })
 
     .controller("MapCtrl", function ($scope) {
         $scope.name = "Map";
+
+        $scope.ol = {};
+        $scope.ol.center = {
+            lat: 51.505,
+            lon: -0.09,
+            zoom: 8
+        };
+
+        $scope.ol.center = {
+            lat: 9797587.027268754,
+            lon: 2629703.3656816175,
+            zoom: 5,
+            projection: "EPSG:3857"
+        };
+
+        $scope.ol.layers = {
+            main: {
+                source: {
+                    type: "OSM"
+                }
+            },
+            statkart: {
+                source: {
+                    type: "XYZ",
+                    url: "http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}"
+                }
+            }
+        }
     });
