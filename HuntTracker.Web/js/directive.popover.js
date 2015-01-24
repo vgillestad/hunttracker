abc181977beaed9f2e671fc7a34aafc67ed712ee
Vegard Gillestad <vegard.gillestad@gmail.com>
2015-01-24 10:52:09 +0100

    finally popover is working

--- HuntTracker.Web/js/directive.popover.js
+++ HuntTracker.Web/js/directive.popover.js
@@ -7,20 +7,18 @@
             scope: {
                 show: "="
             },
-            link: function (scope, element, attrs) {
-                var $target = $(attrs.target);
-                var popover = $target.popover({ 'html': true, content: "this is actual content" }).popover("show");
-                var popoverContent = popover.data("bs.popover").$tip.find(".popover-content").first();
-                popoverContent.html(element.children());
-                element.remove();
-
-                scope.$watch("show", function() {
-                    if (scope.show) {
-                        popover.popover("show");
-                    } else {
-                        popover.popover("hide");
-                    }
-                });
+            compile: function (element, attrs, transcludeFn) {
+                return function postLink(scope, element, attrs, controller) {
+                    var $target = $(attrs.target);
+                    var popover = $target.popover({ 'html': true, content: transcludeFn(scope.$parent) });
+                    scope.$watch("show", function () {
+                        if (scope.show) {
+                            popover.popover("show");
+                        } else {
+                            popover.popover("hide");
+                        }
+                    });
+                };
             }
         };
     });
