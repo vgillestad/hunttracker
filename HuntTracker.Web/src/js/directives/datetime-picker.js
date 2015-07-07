7054d8d4a2a45f49ae46838763538bb2670ff6df
Vegard Gillestad <vegard.gillestad@gmail.com>
2015-07-07 20:18:20 +0200

    date picker now working on ios

--- HuntTracker.Web/src/js/directives/datetime-picker.js
+++ HuntTracker.Web/src/js/directives/datetime-picker.js
@@ -18,37 +18,95 @@
                             '<i class="icon-clock"></i>' +
                         '</span>' +
                     '</div>' +
+                    
                 '</div>',
             replace: true,
             scope: {
                 model: "=model"
             },
             link: function (scope, element, attrs) {
+                var icons = {
+                    time: "fa fa-clock-o",
+                    date: "fa fa-calendar",
+                    up: "icon-arrow-up",
+                    down: "icon-arrow-down",
+                    previous: 'icon-arrow-left',
+                    next: 'icon-arrow-right',
+                    today: 'icon-shrink',
+                    clear: 'icon-bin',
+                };
+
                 var $date = element.find('input[type="date"]');
                 var $time = element.find('input[type="time"]');
 
-                if (!Modernizr.inputtypes.date || !$window.isMobileOrTablet()) {
+                var updateModel = function () {
+                    var date = $date.data("DateTimePicker").date();
+                    var time = $time.data("DateTimePicker").date();
+                    var update = new Date();
+                    update.setDate(date.date());
+                    update.setMonth(date.month());
+                    update.setFullYear(date.year());
+                    update.setHours(time.hour());
+                    update.setMinutes(time.minutes());
+
+                    scope.model = update.toISOString();
+                    scope.$apply();
+                };
+
+                var updateModel2 = function () {
+                    var update = moment($date.val());
+                    var time = $time.val();
+                    update.hour(time.substr(0,2));
+                    update.minutes(time.substr(3,4));
+
+                    scope.model = update.toISOString();
+                    scope.$apply();
+                };
+
+                var useDateInput = Modernizr.inputtypes.date && $window.isMobileOrTablet();
+                var useTimeInput = Modernizr.inputtypes.time && $window.isMobileOrTablet();
+
+                if (useDateInput) {
+                    $date.val(moment(scope.model).format('YYYY-MM-DD'));
+                    $date.on("change", function (e) {
+                        updateModel2();
+                    });
+                }
+                else {
                     $date.attr('type', 'text');
                     $date.datetimepicker({
+                        defaultDate: scope.model,
                         format: 'L',
+                        icons: icons,
                     });
                     $date.siblings().first().on('click', function () {
                         $date.data("DateTimePicker").toggle();
                     });
+                    $date.on("dp.change", function (e) {
+                        updateModel();
+                    });
                 }
 
-                if (!Modernizr.inputtypes.time || !$window.isMobileOrTablet()) {
+                if (useTimeInput) {
+                    $time.val(moment(scope.model).format('HH:mm'));
+                    $time.on("change", function () {
+                        updateModel2();
+                    });
+                }
+                else {
                     $time.attr('type', 'text');
                     $time.datetimepicker({
-                        format: 'LT'
+                        defaultDate: scope.model,
+                        format: 'LT',
+                        icons: icons,
                     });
                     $time.siblings().last().on('click', function () {
                         $time.data("DateTimePicker").toggle();
                     });
+                    $time.on("dp.change", function (e) {
+                        updateModel();
+                    });
                 }
-
-                $date.val(moment(scope.model).format('L'));
-                $time.val(moment(scope.model).format('LT'));
             }
         };
     }]);
