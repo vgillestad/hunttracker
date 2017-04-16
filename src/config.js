64eb319d548848599082dc28846e9872e6ca7cf0
Vegard Gillestad <vegard.gillestad@gmail.com>
2017-04-16 22:19:33 +0200

    logging

--- src/config.js
+++ src/config.js
@@ -6,6 +6,8 @@ try {
 config.port = process.env.PORT || config.port || 3000;
 config.env = process.env.NODE_ENV || config.env || 'production'
 config.jwtSecret = process.env.JWT_SECRET || config.jwtSecret;
-config.postgresConnection = process.env.POSTGRES_CONNECTION || process.env.DATABASE_URL || config.postgresConnection
+config.postgresConnection = process.env.POSTGRES_CONNECTION || process.env.DATABASE_URL || config.postgresConnection;
+
+console.log('using pos:' + config.postgresConnection);
 
 module.exports = config;
