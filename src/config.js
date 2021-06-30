e25b8d3fd2bc1fa388d87293687eba876a8121a0
Vegard Gillestad <vegard.gillestad@gmail.com>
2021-06-30 21:25:11 +0200

    connection with ssl

--- src/config.js
+++ src/config.js
@@ -7,5 +7,6 @@ config.port = process.env.PORT || config.port || 3000;
 config.env = process.env.NODE_ENV || config.env || 'production'
 config.jwtSecret = process.env.JWT_SECRET || config.jwtSecret;
 config.postgresConnection = process.env.POSTGRES_CONNECTION || process.env.DATABASE_URL || config.postgresConnection;
+config.postgresConnection = `${config.postgresConnection}?ssl=true`;
 
 module.exports = config;
